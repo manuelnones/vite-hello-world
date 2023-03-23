755aa054b2151fc766b397c2dea7103f290f2f09
manuelnones <manuelnones99@yahoo.com>
2023-03-23 19:34:46 +0100

    fin exercise

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            title: `La mia prima app con Vite!`,
+        }
+    }
+}
+</script>
+
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+
+<style scoped>
+    h1 {
+        width: 600px;
+        margin: auto;
+        font-size: 3em;
+        color: #E734F2;
+    }
+</style>
